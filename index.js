98dc069a7b1ee9b663a11eb6816dccff17c77583
deepen77 <przemek.starostka@yahoo.com>
2021-04-04 00:19:59 +0100

    gallery slider

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
+var swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
 const navBar = document.querySelector(".hamburger");
 const navMobileItems = document.querySelector(".menu-list-mobile");
-const backroundImageMobile = document.querySelector(".backgroundImage-lena-mobile");
+const backroundImageMobile = document.querySelector(
+  ".backgroundImage-lena-mobile"
+);
 
 navBar.addEventListener("click", () => {
   navBar.classList.toggle("is-active");
   navMobileItems.classList.toggle("open");
   backroundImageMobile.classList.toggle("open");
-
 });
+
+let modalContainer = document.querySelector(".swiper-container");
+let modal = document.querySelector(".swiper-wrapper");
+const button = document.querySelectorAll(".gallery img");
+const buttonClose = document.querySelector(".fa-window-close");
+
+// function openModal() {
+//   modalContainer.classList.add("open");
+//   modal.classList.add("show");
+//   modal.classList.remove("close");
+//   swiper.update();
+// }
+
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", (e) => {
+  modalContainer.classList.add("open");
+  modal.classList.add("show");
+  modal.classList.remove("close");
+  // var $invoker = $(e.relatedTarget);
+  // swiper.slideTo($invoker.data("slider"));
+  swiper.update();
+})
+}
+
+
+function closeModal() {
+  modal.classList.remove("show");
+  modal.classList.add("close");
+  modalContainer.classList.remove("open");
+  swiper.update();
+  // setTimeout(() => modalContainer.classList.remove("open"), 500);
+}
+
+// button.addEventListener("click", openModal);
+buttonClose.addEventListener("click", closeModal);
 
 // const navItems = document.querySelectorAll(".nav_item");
 
