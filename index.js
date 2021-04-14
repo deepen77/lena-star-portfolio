da0e11d1c29cf2a24e4a4470da1b20b54ae0ad4d
deepen77 <przemek.starostka@yahoo.com>
2021-04-14 22:33:50 +0100

    server created for contact form

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const menuItem = document.querySelectorAll(".menu-item");
 const backroundImageMobile = document.querySelector(".backgroundImage-lena-mobile");
 
 
+const contactForm = document.querySelector(".contact-form");
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let subject = document.getElementById("subject");
+let message = document.getElementById("message");
+
+
+
+
 
 navBar.addEventListener("click", () => {
   navBar.classList.toggle("is-active");
@@ -25,3 +34,31 @@ function navSettings() {
 navSettings();
 
 
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let formData = {
+    name: name.value,
+    email: email.value,
+    subject: subject.value,
+    message: message.value,
+  };
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/");
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.onload = function () {
+    console.log(xhr.responseText);
+    if (xhr.responseText == "success") {
+      alert("Email sent");
+      name.value = "";
+      email.value = "";
+      subject.value = "";
+      message.value = "";
+    } else {
+      alert("Something went wrong!");
+    }
+  };
+
+  xhr.send(JSON.stringify(formData));
+});
