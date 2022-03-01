47b379bbdd13e0631bf2fe0ad467240735c6a6b8
deepen77 <przemek.starostka@yahoo.com>
2022-03-01 17:07:07 +0100

    about section path added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const navMobileItems = document.querySelector(".menu-list-mobile");
 const menuItem = document.querySelectorAll(".menu-item");
 const backroundImageMobile = document.querySelector(".backgroundImage-lena-mobile");
 
-/*
+
 const contactForm = document.querySelector(".contact-form");
 let name = document.getElementById("name");
 let email = document.getElementById("email");
@@ -15,7 +15,7 @@ const closeModalError = document.querySelector(".close-modal");
 
 const modalMessageSuccess = document.querySelector(".modal-message-success");
 const closeModalSuccess = document.querySelector(".close-modal-success");
-*/
+
 
 
 navBar.addEventListener("click", () => {
@@ -32,45 +32,13 @@ function navSettings() {
     menuItem.forEach((li) => {
       li.classList.toggle("style");
     });
-  } 
+  }
 }
 
 navSettings();
 
-/*
-contactForm.addEventListener("submit", (e) => {
-  e.preventDefault();
 
-  let formData = {
-    name: name.value,
-    email: email.value,
-    subject: subject.value,
-    message: message.value,
-  };
 
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "/");
-  xhr.setRequestHeader("content-type", "application/json");
-  xhr.onload = function () {
-    console.log(xhr.responseText);
-    if (xhr.responseText == "success") {
-      // alert("Email sent");
-      modalMessageSuccess.classList.toggle("open-modal-message-success");
-      name.value = "";
-      email.value = "";
-      subject.value = "";
-      message.value = "";
-    } else {
-      // alert("Something went wrong!");
-      modalMessageError.classList.toggle("open-modal-message-error");
-    }
-  };
-
-  xhr.send(JSON.stringify(formData));
-});
-*/
-
-/*
 closeModalError.addEventListener('click', () => {
   modalMessageError.classList.toggle("open-modal-message-error");
 })
@@ -79,4 +47,4 @@ closeModalSuccess.addEventListener("click", () => {
   modalMessageSuccess.classList.toggle("open-modal-message-success");
 });
 
-*/
+
